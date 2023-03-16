3040b6962de764d238fc6f4571048cdf039a1d8a
henryfeng <huili.f@gmail.com>
2023-03-16 09:14:35 +0800

    add class to form layout

--- src/lib/Col.svelte
+++ src/lib/Col.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     export let span:number = 1;
     export let style = '';
+    let className: string = '';
+    export {className as class}
 </script>
-<div class="tsui-form-col span-{span}" {style}>
+<div class="tsui-form-col span-{span} {className}" {style}>
     <slot></slot>
 </div>
--- src/lib/Row.svelte
+++ src/lib/Row.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     export let style = '';
+    let className: string = '';
+    export {className as class}
 </script>
-<div class="tsui-form-row" {style}>
+<div class="tsui-form-row {className}" {style}>
     <slot></slot>
 </div>
